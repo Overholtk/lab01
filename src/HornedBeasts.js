0cbdcde16808b8cbaa0e47abc436373d05b72ea2
KAYA OVERHOLT <OVERHOLTKAYA@GMAIL.COM>
2022-01-16 20:38:26 -0800

    finished lab 2 requirements

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,16 +1,35 @@
 import React from "react";
+import './HornedBeasts.css'
+import Card from 'react-bootstrap/Card'
 
 class HornedBeasts extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            "favorites": 0
+        }
+    }
+    
+    handleClick = () => {
+        this.setState({
+            favorites: this.state.favorites + 1
+        });
+    }
+    
     render(){
         return(
-            <article>
-                <h2>{this.props.name}</h2>
-                <img 
+            <Card className="beast">
+                <Card.Img 
                     src={this.props.imageUrl} 
-                    alt={this.props.name} 
-                    title={this.props.name}/>
-                <p>{this.props.description}</p>
-            </article>
+                    alt={this.props.title}
+                    onClick={this.handleClick}
+                    />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>{this.props.description}</Card.Text>
+                    <Card.Text>💖: {this.state.favorites}</Card.Text>
+                </Card.Body>
+            </Card>
         )
     }
 }
